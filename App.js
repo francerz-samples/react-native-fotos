ae99e1bc37531b4b53aa4c18541531829b1bdd5f
Francerz <francerzam@hotmail.com>
2021-06-15 16:11:41 -0500

    ADD camara compatibility

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 import { Alert, Button, Image, View } from 'react-native';
 import { launchImageLibrary } from 'react-native-image-picker'
+import { RNCamera } from 'react-native-camera';
 
-
+const androidCameraPermissionOptions = {
+  title: "Permitir acceso a la cámara",
+  message: "¿Desea permitir el acceso a la cámara?",
+  buttonPositive: "Sí",
+  buttonNegative: "No"
+}
 
 const App = function() {
 
@@ -28,12 +34,28 @@ const App = function() {
     })
   }
 
+  const capturarOnPress = async function() {
+    if (this.camara) {
+      const options = {quality: .5, base64:true}
+      const data = await this.camara.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  }
+
   return (
     <View>
       <Button title="Abrir galería" onPress={() => openGallery()} />
       <Image source={photoSource}
         style={{height:300}}
         onError={(error) => console.log(error)} />
+      <RNCamera
+        ref={ref => { this.camara = ref }}
+        style={{height: 300}}
+        type={RNCamera.Constants.Type.back}
+        captureAudio={false}
+        androidCameraPermissionOptions={androidCameraPermissionOptions}
+      />
+      <Button title="Capturar" onPress={capturarOnPress} />
     </View>
   )
 }
